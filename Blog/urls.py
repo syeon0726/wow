8afe78b49ccee77bb7bc8dd9e6d92dd674150854
syeon0726 <hyk4648@naver.com>
2022-05-14 16:10:43 +0900

    third

---
+++ Blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    # path('', views.index, name='page-list'),
+    # path('create/', views.create, name='page-create'),
+    # path('update/page/<int:page_id>/', views.update, name='page-update'),
+    # path('delete/page/<int:page_id>/', views.delete, name='page-delete'),
+    # path('read/page/<int:page_id>', views.read, name='page-read')
+
+]
--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
-
-from mysite.Blog.models import Blog
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import PageForm
+from .models import Page
 
 # Create your views here.
 
-def home(request) :
-    blogs = Blog.objects
-    return render(request, 'home.html')
 
-def new(request):
-    return render(request, 'new.html')
+def index(request):
+    object_list = Page.objects.all()
+    return render(request, 'Blog/home.html', {'object_list': object_list})
+
+
+def read(request, page_id):
+    object_detail = get_object_or_404(Page, id=page_id)
+    return render(request, 'Blog/page_detail.html', {'object_detail': object_detail})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('page-read', page_id=form.id)
+    else:
+        form = PageForm()
+        return render(request, 'Blog/page_create.html', {'form': form})
+
+
+def update(request, page_id):
+    update_content = get_object_or_404(Page, id=page_id)
+
+    if request.method == 'POST':
+        form = PageForm(request.POST, instance=update_content)
+        if form.is_valid():
+            form.save()
+            return redirect('page-read', page_id=update_content.id)
+    else:
+        form = PageForm(instance=update_content)
+    return render(request, 'Blog/page_create.html', {'form': form})
+
+
+def delete(request, page_id):
+    object = get_object_or_404(Page, id=page_id)
+
+    if request.method == 'POST':
+        object.delete()
+        return redirect('page-list')
+    else:
+        return render(request, 'Blog/page_confirm.html', {'object': object})
